7e17b417a93768e67117e174ae849c8a84f3c6b9
myc37 <nvjn37@gmail.com>
2022-01-07 20:25:58 +0800

    set up transaction modal

--- src/Components/DashboardCardTransactionHistory.js
+++ src/Components/DashboardCardTransactionHistory.js
@@ -1,20 +1,32 @@
 import TransactionHistory from "./TransactionHistory";
 import "./DashboardCard.css";
+import { useState } from "react";
+import Transaction from "../Pages/Transaction";
 
 export const DashboardCardTransactionHistory = () => {
-  return (
-    <div className="dashboardcard-transaction-box">
-      <div className="dashboardcard-header-box">
-        <div className="flex justify-between items-center">
-          <h2 className="dashboardcard-header-transaction">Transactions</h2>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-md">
-            Add a transaction
-          </button>
-        </div>
-        <div className="dashboardcard-subheader">Past 7 days</div>
-        <div className="dashboardcard-amount">Amount</div>
-      </div>
-      <TransactionHistory />
-    </div>
-  );
+	const [open, setOpen] = useState(false);
+	const props = { open, setOpen };
+	return (
+		<>
+			<div className="dashboardcard-transaction-box">
+				<div className="dashboardcard-header-box">
+					<div className="flex justify-between items-center">
+						<h2 className="dashboardcard-header-transaction">
+							Transactions
+						</h2>
+						<button
+							className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-md"
+							onClick={() => setOpen(true)}
+						>
+							Add a transaction
+						</button>
+					</div>
+					<div className="dashboardcard-subheader">Past 7 days</div>
+					<div className="dashboardcard-amount">Amount</div>
+				</div>
+				<TransactionHistory />
+			</div>
+			<Transaction {...props} />
+		</>
+	);
 };
--- src/Pages/Transaction.js
+++ src/Pages/Transaction.js
@@ -4,13 +4,21 @@ import { db } from "../firebase";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { toast } from "react-toastify";
+import {
+	Dialog,
+	DialogTitle,
+	DialogContent,
+	DialogActions,
+} from "@mui/material";
+import { useAuth } from "../Contexts/AuthContext";
 
 const expenseOptions = ["Transport", "Food", "Luxury", "Business", "Others"];
 
-const Transaction = () => {
+const Transaction = ({ open, setOpen }) => {
+	const { currentUser } = useAuth();
 	const [date, setDate] = useState(new Date());
 	const [type, setType] = useState("income");
-	const [amount, setAmount] = useState(0.0);
+	const [amount, setAmount] = useState();
 	const [category, setCategory] = useState("Transport");
 	const [description, setDescription] = useState("");
 	const nav = useNavigate();
@@ -26,6 +34,7 @@ const Transaction = () => {
 					: parseFloat("-" + amount),
 			category,
 			description,
+			user: currentUser ? currentUser.uid : "",
 		};
 		console.log(formData);
 		try {
@@ -45,124 +54,138 @@ const Transaction = () => {
 	}
 
 	return (
-		<>
-			<h1 className="w-full text-center font-semibold text-4xl my-4">
-				Add Transaction
-			</h1>
-			<form
-				onSubmit={handleSubmit}
-				className="grid grid-rows-auto gap-4 place-content-center"
-			>
-				<div className="mt-4">
-					<label
-						htmlFor="datepicker"
-						className="block font-semibold text-blue-700 mb-2"
-					>
-						Date:
-					</label>
-					<DatePicker
-						name="datepicker"
-						id="datepicker"
-						selected={date}
-						onChange={(e) => setDate(e)}
-						className="p-2 rounded-md border-2 w-full text-center"
-					/>
-				</div>
-				<div>
-					<label
-						htmlFor="type"
-						className="block font-semibold text-blue-700 mb-2"
-					>
-						Type:
-					</label>
-					<select
-						name="type"
-						id="type"
-						value={type}
-						onChange={(e) => {
-							setType(e.target.value);
-						}}
-						className="rounded-md border-2 w-full p-2 text-center"
-						required
-					>
-						<option value="">--Choose--</option>
-						<option value="income">Income</option>
-						<option value="expense">Expense</option>
-					</select>
-				</div>
+		<Dialog open={open} onClose={() => setOpen(false)}>
+			<DialogTitle>
+				<h1 className="w-full text-center font-semibold text-4xl my-4">
+					Add Transaction
+				</h1>
+			</DialogTitle>
+			<DialogContent>
+				<form
+					id="transactionForm"
+					className="grid grid-rows-auto gap-4 place-content-center"
+					onSubmit={handleSubmit}
+				>
+					<div className="mt-4">
+						<label
+							htmlFor="datepicker"
+							className="block font-semibold text-blue-700 mb-2"
+						>
+							Date:
+						</label>
+						<DatePicker
+							name="datepicker"
+							id="datepicker"
+							selected={date}
+							onChange={(e) => setDate(e)}
+							className="p-2 rounded-md border-2 border-gray-400 w-full text-center"
+						/>
+					</div>
+					<div>
+						<label
+							htmlFor="type"
+							className="block font-semibold text-blue-700 mb-2"
+						>
+							Type:
+						</label>
+						<select
+							name="type"
+							id="type"
+							value={type}
+							onChange={(e) => {
+								setType(e.target.value);
+							}}
+							className="rounded-md border-2 border-gray-400 w-full p-2 text-center"
+							required
+						>
+							<option value="">--Choose--</option>
+							<option value="income">Income</option>
+							<option value="expense">Expense</option>
+						</select>
+					</div>
 
-				<div>
-					<label
-						htmlFor="category"
-						className="block font-semibold text-blue-700 mb-2"
-					>
-						Category:
-					</label>
-					<select
-						name="category"
-						id="category"
-						onChange={(e) => setCategory(e.target.value)}
-						disabled={type !== "expense"}
-						className="rounded-md border-2 w-full p-2 text-center"
-						required={type === "expense"}
-					>
-						{type === "expense" &&
-							expenseOptions.map((option, index) => {
-								return (
-									<option key={index} value={option}>
-										{option}
-									</option>
-								);
-							})}
-						{type !== "expense" && <option>--N.A.--</option>}
-					</select>
-				</div>
+					<div>
+						<label
+							htmlFor="category"
+							className="block font-semibold text-blue-700 mb-2"
+						>
+							Category:
+						</label>
+						<select
+							name="category"
+							id="category"
+							onChange={(e) => setCategory(e.target.value)}
+							disabled={type !== "expense"}
+							className="rounded-md border-2 border-gray-400 w-full p-2 text-center"
+							required={type === "expense"}
+						>
+							{type === "expense" &&
+								expenseOptions.map((option, index) => {
+									return (
+										<option key={index} value={option}>
+											{option}
+										</option>
+									);
+								})}
+							{type !== "expense" && <option>--N.A.--</option>}
+						</select>
+					</div>
 
-				<div className="w-full">
-					<label
-						htmlFor="amount"
-						className="block font-semibold text-blue-700 mb-2"
-					>
-						Amount:
-					</label>
-					<input
-						name="amount"
-						id="amount"
-						type="number"
-						step="0.01"
-						min="0"
-						value={amount}
-						onChange={(e) => setAmount(e.target.value)}
-						className="rounded-md border-2 p-2 text-center w-full"
-						required
-					/>
-				</div>
+					<div className="w-full">
+						<label
+							htmlFor="amount"
+							className="block font-semibold text-blue-700 mb-2"
+						>
+							Amount:
+						</label>
+						<input
+							name="amount"
+							id="amount"
+							type="number"
+							step="0.01"
+							min="0"
+							value={amount}
+							onChange={(e) => setAmount(e.target.value)}
+							className="rounded-md border-2 border-gray-400 p-2 text-center w-full"
+							required
+						/>
+					</div>
 
-				<div className="w-full">
-					<label
-						htmlFor="description"
-						className="block font-semibold text-blue-700 mb-2"
-					>
-						Description:
-					</label>
-					<textarea
-						name="description"
-						id="description"
-						rows="5"
-						cols="50"
-						value={description}
-						onChange={(e) => setDescription(e.target.value)}
-						className="border-2 rounded-md p-2"
-					></textarea>
-				</div>
-
+					<div className="w-full">
+						<label
+							htmlFor="description"
+							className="block font-semibold text-blue-700 mb-2"
+						>
+							Description:
+						</label>
+						<textarea
+							name="description"
+							id="description"
+							rows="5"
+							cols="50"
+							value={description}
+							onChange={(e) => setDescription(e.target.value)}
+							className="border-2 border-gray-400 rounded-md p-2"
+						></textarea>
+					</div>
+				</form>
+			</DialogContent>
+			<DialogActions>
 				<input
 					type="submit"
+					form="transactionForm"
 					value="Submit"
-					className="rounded-lg bg-blue-500 px-4 py-2 w-fit m-auto text-white text-sm hover:cursor-pointer"
+					className="rounded-lg bg-blue-500 px-4 py-2 w-fit text-white text-lg hover:cursor-pointer hover:bg-blue-400"
 				/>
-			</form>
-		</>
+
+				<button
+					onClick={() => setOpen(false)}
+					className="rounded-lg bg-red-500 px-4 py-2 w-fit text-white text-lg hover:cursor-pointer hover:bg-red-400 ml-2"
+				>
+					Close
+				</button>
+			</DialogActions>
+		</Dialog>
 	);
 };
 
