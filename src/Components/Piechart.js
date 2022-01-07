27f39e0068107203a8ee9470a1686c4adf08566b
ben-dom393 <77182164+ben-dom393@users.noreply.github.com>
2022-01-07 23:27:17 +0800

    fetched firebase data

--- src/Components/Piechart.js
+++ src/Components/Piechart.js
@@ -9,8 +9,16 @@ import {
 } from "recharts";
 
 
-const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
-export default function Piechart() {
+
+export default function Piechart({ transactions, today, oneWeekAgo }) {
+  const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042","FF0000"];
+  const data01 = [
+    {  name: 'Transport', value: transactions.filter(transaction => transaction.category ==="Transport" && transaction.type ==="expense").map(filtered => -filtered.amount).reduce((a,b)=> a+b,0)},
+    { name: 'Food', value: transactions.filter(transaction => transaction.category ==="Food" && transaction.type ==="expense").map(filtered => -filtered.amount).reduce((a,b)=> a+b,0) },
+    { name: 'Luxury', value: transactions.filter(transaction => transaction.category ==="Luxury" && transaction.type ==="expense").map(filtered => -filtered.amount).reduce((a,b)=> a+b,0)},
+    { name: 'Business', value: transactions.filter(transaction => transaction.category ==="Business" && transaction.type ==="expense").map(filtered => -filtered.amount).reduce((a,b)=> a+b,0) },
+    { name: 'Others', value: transactions.filter(transaction => transaction.category ==="Others" && transaction.type ==="expense").map(filtered => -filtered.amount).reduce((a,b)=> a+b,0) },
+  ];
   return (
     <ResponsiveContainer width="100%" aspect={3}>
       <PieChart title="Test" width={400} height={400}>
@@ -23,9 +31,9 @@ export default function Piechart() {
           innerRadius={60}
           outerRadius={80}
           fill="#8884d8"
-          label
+         
         >
-          {data01.map((entry, index) => (
+          {transactions.map((entry, index) => (
             <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
           ))}
         </Pie>
