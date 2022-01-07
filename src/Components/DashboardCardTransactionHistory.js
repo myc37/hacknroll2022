2900a29eae20ceacd3d525fcd84a1d3e7bd8dd20
Timothy <timcheng112@gmail.com>
2022-01-07 20:28:56 +0800

    Updated Dashboard

--- src/Components/DashboardCardTransactionHistory.js
+++ src/Components/DashboardCardTransactionHistory.js
@@ -1,13 +1,20 @@
 import TransactionHistory from "./TransactionHistory";
 import "./DashboardCard.css";
+import { useState } from "react";
 
-export const DashboardCardTransactionHistory = () => {
+export const DashboardCardTransactionHistory = ({ data }) => {
+  const [open, setOpen] = useState(false);
+  console.log(data);
+
   return (
     <div className="dashboardcard-transaction-box">
       <div className="dashboardcard-header-box">
         <div className="flex justify-between items-center">
           <h2 className="dashboardcard-header-transaction">Transactions</h2>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-md">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-md"
+            onClick={() => setOpen(true)}
+          >
             Add a transaction
           </button>
         </div>
--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,33 +1,63 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { DashboardCardBarchart } from "../Components/DashboardCardBarchart";
 import { DashboardCardLinechart } from "../Components/DashboardCardLinechart";
 import { DashboardCardPiechart } from "../Components/DashboardCardPiechart";
 import { DashboardCardTransactionHistory } from "../Components/DashboardCardTransactionHistory";
+import firebase from "../firebase";
+import { useAuth } from "../Contexts/AuthContext";
 // import { db } from "../firebase";
 
 const Dashboard = () => {
-	useEffect(() => {
-		// async function fetchData() {
-		// 	await db
-		// 		.collection("transactions")
-		// 		.get()
-		// 		.then((res) => console.log(res.docs));
-		// }
-		// fetchData();
-	}, []);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const { currentUser } = useAuth();
 
-	// [{date: 01/07/2022, category: transport, amount: 100, description: ""}]
+  function getTransactionsByUid() {
+    const tempData = [];
+    data.filter((data) =>
+      currentUser.uid === data.user ? tempData.push(data) : console.log("")
+    );
+    setData(tempData);
+  }
 
-	return (
-		<div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
-			<div className="grid grid-cols-8 gap-4">
-				<DashboardCardTransactionHistory />
-				<DashboardCardBarchart />
-				<DashboardCardPiechart />
-				<DashboardCardLinechart />
-			</div>
-		</div>
-	);
+  const ref = firebase.firestore().collection("transactions");
+
+  function getData() {
+    setLoading(true);
+    ref.onSnapshot((querySnapshot) => {
+      const items = [];
+      querySnapshot.forEach((doc) => {
+        items.push(doc.data());
+      });
+      setData(items);
+      getTransactionsByUid();
+      setLoading(false);
+    });
+  }
+
+  useEffect(() => {
+    // async function fetchData() {
+    // 	await db
+    // 		.collection("transactions")
+    // 		.get()
+    // 		.then((res) => console.log(res.docs));
+    // }
+    // fetchData();
+    getData();
+  }, []);
+
+  // [{date: 01/07/2022, category: transport, amount: 100, description: ""}]
+
+  return (
+    <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+      <div className="grid grid-cols-8 gap-4">
+        <DashboardCardTransactionHistory data={data} />
+        <DashboardCardBarchart />
+        <DashboardCardPiechart />
+        <DashboardCardLinechart />
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
