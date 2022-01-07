15e38268f534767b5d68506cda7987549ef072a7
keithcharlesc <keithcharleschan@gmail.com>
2022-01-07 13:44:25 +0800

    Registration and Login components

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../Contexts/AuthContext";
 
 const Login = () => {
-	return <div>Login</div>;
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const [loading, setLoading] = useState(false);
+  const { login, currentUser } = useAuth();
+  const nav = useNavigate();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      await login(emailRef.current.value, passwordRef.current.value);
+      //   nav("/dashboard");
+      //   console.log(currentUser.email);
+    } catch (error) {
+      console.log("error");
+    }
+    setLoading(false);
+  }
+
+  return (
+    <>
+      <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <form className="flex-col" onSubmit={handleSubmit}>
+          {/* <h1>{currentUser.email}</h1> */}
+          <h2 className="text-lg font-bold mb-4 text-blue-700">Login</h2>
+          <label className="font-semibold" htmlFor="email">
+            Email address
+          </label>
+          <input
+            type="email"
+            placeholder="Email address"
+            name="email"
+            className="py-0.5 display: block mb-4 border-solid border-2 placeholder-gray-500 text-gray-900 rounded-lg focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+            ref={emailRef}
+          />
+          <label className="font-semibold" htmlFor="email">
+            Password
+          </label>
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            className="py-0.5 display: block mb-4 border-solid border-2 placeholder-gray-500 text-gray-900 rounded-lg focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+            ref={passwordRef}
+          />
+
+          <input
+            className="bg-blue-500 rounded-md px-4 py-2 text-sm text-white hover:cursor-pointer"
+            type="submit"
+            value="Submit"
+            disabled={loading}
+          />
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default Login;
--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,55 +1,76 @@
 import React, { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../Contexts/AuthContext";
+import "./Register.css";
 
 const Register = () => {
-	const emailRef = useRef();
-	const passwordRef = useRef();
-	const confirmRef = useRef();
-	const [loading, setLoading] = useState(false);
-	const { register } = useAuth();
-	const nav = useNavigate();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const confirmRef = useRef();
+  const [loading, setLoading] = useState(false);
+  const { register } = useAuth();
+  const nav = useNavigate();
 
-	async function handleSubmit(e) {
-		e.preventDefault();
+  async function handleSubmit(e) {
+    e.preventDefault();
 
-		if (passwordRef.current.value !== confirmRef.current.value) return;
+    if (passwordRef.current.value !== confirmRef.current.value) return;
 
-		try {
-			setLoading(true);
-			await register(emailRef.current.value, passwordRef.current.value);
-			nav("/login");
-		} catch (error) {
-			console.log("error");
-		}
-		setLoading(false);
-	}
+    try {
+      setLoading(true);
+      await register(emailRef.current.value, passwordRef.current.value);
+      nav("/login");
+    } catch (error) {
+      console.log("error");
+    }
+    setLoading(false);
+  }
 
-	return (
-		<div className="bg-yellow-500">
-			<form onSubmit={handleSubmit}>
-				<input
-					type="email"
-					name="email"
-					className="bg-blue-200"
-					ref={emailRef}
-				/>
-				<input
-					type="password"
-					name="password"
-					className="bg-red-200"
-					ref={passwordRef}
-				/>
-				<input
-					type="password"
-					name="confirm"
-					className="bg-green-200"
-					ref={confirmRef}
-				/>
-				<input type="submit" value="submit" disabled={loading} />
-			</form>
-		</div>
-	);
+  return (
+    <>
+      <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <form className="flex-col" onSubmit={handleSubmit}>
+          <h2 className="text-lg font-bold mb-4 text-blue-700">Registration</h2>
+          <label className="font-semibold" htmlFor="email">
+            Email address
+          </label>
+          <input
+            type="email"
+            placeholder="Email address"
+            name="email"
+            className="py-0.5 display: block mb-4 border-solid border-2 placeholder-gray-500 text-gray-900 rounded-lg focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+            ref={emailRef}
+          />
+          <label className="font-semibold" htmlFor="email">
+            Password
+          </label>
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            className="py-0.5 display: block mb-4 border-solid border-2 placeholder-gray-500 text-gray-900 rounded-lg focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+            ref={passwordRef}
+          />
+          <label className="font-semibold" htmlFor="confirm">
+            Confirm Password
+          </label>
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="confirm"
+            className="py-0.5 display: block mb-4 border-solid border-2 placeholder-gray-500 text-gray-900 rounded-lg focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+            ref={confirmRef}
+          />
+          <input
+            className="bg-blue-500 rounded-md px-4 py-2 text-sm text-white hover:cursor-pointer"
+            type="submit"
+            value="Submit"
+            disabled={loading}
+          />
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default Register;
