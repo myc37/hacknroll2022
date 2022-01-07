6269168fda7b8d3b5588d1c5f5f8acfad0b1db0b
Timothy <timcheng112@gmail.com>
2022-01-07 18:15:50 +0800

    Updated DashboardCardTransactionHistory

--- src/Components/DashboardCardTransactionHistory.js
+++ src/Components/DashboardCardTransactionHistory.js
@@ -7,7 +7,7 @@ export const DashboardCardTransactionHistory = () => {
       <div className="dashboardcard-header-box">
         <div className="flex justify-between items-center">
           <h2 className="dashboardcard-header-transaction">Transactions</h2>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-md ">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-md">
             Add a transaction
           </button>
         </div>
--- src/Components/Piechart.js
+++ src/Components/Piechart.js
@@ -44,47 +44,8 @@ export default function Piechart() {
           dominantBaseline="central"
         ></text>
         <Legend />
-
-<<<<<<< HEAD
         <Tooltip />
       </PieChart>
     </ResponsiveContainer>
   );
 }
-=======
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
-export default class Example extends PureComponent {
-  static demoUrl = 'https://codesandbox.io/s/two-simple-pie-chart-otx9h';
-
-  render() {
-    return (
-    
-      <ResponsiveContainer width="100%" aspect={3}>
-        <PieChart title="Test" width={400} height={400}>
-          <Pie
-            dataKey="value"
-            isAnimationActive={false}
-            data={data01}
-            cx="50%"
-            cy="50%"
-            innerRadius={60}
-            outerRadius={80}
-            fill="#8884d8"
-          >
-              {data01.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-            ))}
-
-
-          </Pie>
-          
-          <Legend />
-     
-          <Tooltip />
-        </PieChart>
-      </ResponsiveContainer>
-    
-    );
-  }
-}
->>>>>>> af013ce4d4b7a8e063049c2a12ebbebb271e40f0
