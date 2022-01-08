53063c357bbd0897e91d5bbd099ea9343a9eab96
keithcharlesc <keithcharleschan@gmail.com>
2022-01-08 08:00:12 +0800

    Updated design features

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -12,21 +12,21 @@ const Home = () => {
   return (
     <div className="bg-white h-screen flex flex-row justify-center items-center">
       <div className="max-w-md">
-        <h1 className="whitespace-nowrap lg:text-6xl md:text-7xl sm:text-5xl text-3xl font-black mb-3 ">
+        <h1 className="whitespace-nowrap lg:text-6xl md:text-7xl sm:text-5xl text-3xl font-black mb-4 ">
           FinanceMeister
         </h1>
-        <p className="mb-8">
-          A website to help track one's transactions (expenses and income).
+        <p className="mb-8 ">
+          A website to help track your transactions : expenses & income.
           Provides some useful analysis based on the transactions.
         </p>
         <Link
-          className="text-black font-semibold py-2 px-4 bg-blue-300 rounded-full text-md hover:bg-blue-400 transition duration-300 ease-in-out items-center"
+          className="text-black font-bold py-2 px-4 bg-sky-300 rounded-full text-md hover:bg-blue-400 transition duration-300 ease-in-out items-center"
           to="/register"
         >
           Sign Up!{" "}
         </Link>
       </div>
-      <img src={FinanceBro} alt="Landing Page" className="h-3/4" />
+      <img src={FinanceBro} alt="Landing Page" className="h-3/4 ml-3" />
     </div>
   );
 };
--- src/Pages/Landing/components/Footer.js
+++ src/Pages/Landing/components/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="flex justify-center items-center p-3 bg-black text-white">
-      <p>Copyright © 2022 FinanceMeister All rights reserved. </p>
+    <div className="flex justify-center items-center p-3 bg-secondary text-white">
+      <p>Copyright © 2022 FinanceMeister | All rights reserved. </p>
     </div>
   );
 };
